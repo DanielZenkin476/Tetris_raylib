4437449246441553a64f51ef70ebbd5412224118
SirZen476 <danielzenkin82@gmail.com>
2024-08-31 14:19:02 +0300

    changed backround color

--- Tetris_raylib.cpp
+++ Tetris_raylib.cpp
@@ -8,10 +8,13 @@ int main()
 {
     InitWindow(500, 600, "raylib Tetris");// open window size 300x600 
     SetTargetFPS(60);// must set so game will run- otherwise stuck at startuo - BEFORE GAME LOOP
-
+    // Struct color = {R,G,B,alpha} - 0-255 values, alpha is transparancy
+    Color darkBlue = { 50,50,127,255 };
     while (WindowShouldClose() == false)// will run until esc key is pressed
     {
         BeginDrawing();//creates blank canvas so we can draw
+        ClearBackground(darkBlue);// change backround color
+
 
         EndDrawing();// end canvas drawing
     }
